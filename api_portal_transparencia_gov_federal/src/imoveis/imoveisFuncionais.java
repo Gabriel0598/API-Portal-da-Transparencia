eb84c36f957c3db11b9d86c44ff9f760dbc63822
Gabriel Augusto Ferreira <gabrielaugustoferreira@outlook.com>
2024-10-29 01:44:27 -0300

    C4: Class related to imoveisFuncionais of deputies

--- api_portal_transparencia_gov_federal/src/imoveis/imoveisFuncionais.java
+++ api_portal_transparencia_gov_federal/src/imoveis/imoveisFuncionais.java
@@ -1,2 +1,41 @@
-package imoveis;public class getImoveisFuncionais {
+package imoveis;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Properties;
+
+public class imoveisFuncionais {
+    public static void main(String[] args) throws IOException {
+        // Properties of connection
+        String pathAppProperties = "app.properties";
+        Properties appProps = new Properties();
+        appProps.load(new FileInputStream(pathAppProperties));
+
+        // API Access Key
+        String keyAPI = appProps.getProperty("APIKey");
+
+        // Connection
+        String URLSituacaoImovel = "https://api.portaldatransparencia.gov.br/api-de-dados/situacao-imovel";
+        URL url = new URL(URLSituacaoImovel);
+
+        // Invoke get method
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("Accept", "application/json");
+        connection.setRequestProperty("chave-api-dados", keyAPI);
+
+        // Output buffered
+        BufferedReader buffer = new BufferedReader(
+                new InputStreamReader(connection.getInputStream())
+        );
+        String output;
+        while ((output = buffer.readLine()) != null){
+            System.out.println(output);
+        }
+        connection.disconnect();
+    }
 }
